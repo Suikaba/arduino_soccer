7a0e5cb5ad69e2a8926ed815cb535688ff86f7e6
Suikaba <suibaka@hotmail.co.jp>
2014-01-25 23:27:11 +0900

    Change near ir pin no

--- IR_near_test/IR_near_test.ino
+++ IR_near_test/IR_near_test.ino
@@ -1,9 +1,9 @@
 
 #define NEAR_IR_NUM  8
 
-int near_ir_pin[NEAR_IR_NUM] = {0, 1, 2, 3, 4, 5, 6, 7};
+int near_ir_pin[NEAR_IR_NUM] = {0, 1, 2, 8, 4, 5, 6, 7};
 
-void setup(){
+void setup() {
   Serial.begin(9600);
 }
 
@@ -13,6 +13,7 @@ void loop(){
     val = val * 100 /1023 ;
     Serial.print(val); Serial.print(" ");
   }
-  Serial.println();
+  unsigned long tmp = analogRead(8);
+  Serial.println(tmp);
   delay(100);
 }
