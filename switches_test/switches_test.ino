992e43678e461d518d0bc3fa76b664a42f5af2a5
Suikaba <suibaka@hotmail.co.jp>
2014-01-25 23:35:31 +0900

    Add switch test

---
+++ switches_test/switches_test.ino
@@ -0,0 +1,19 @@
+
+const int SWITCH1_PIN = 48;
+const int SWITCH2_PIN = 50;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(SWITCH1_PIN, INPUT);
+    pinMode(SWITCH2_PIN, INPUT);
+}
+
+void loop() {
+    int switch1 = digitalRead(SWITCH1_PIN);
+    int switch2 = digitalRead(SWITCH2_PIN);
+    Serial.print("SW1:");
+    Serial.print(switch1);
+    Serial.print(" SW2:");
+    Serial.println(switch2);
+    delay(200);
+}
