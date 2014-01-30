e661ed80ec1df0e7725701c588d308968fdaa8a2
Suikaba <suibaka@hotmail.co.jp>
2014-01-30 17:40:03 +0900

    digital --> analog

--- light_sensor/light_sensor.ino
+++ light_sensor/light_sensor.ino
@@ -1,29 +1,33 @@
 
-int pins1[4] = {43, 41, 39, 37};
-int pins2[4] = {36, 38, 40, 42};
-int pins3[4] = {44, 46, 47, 45};
+const int PIN_NUM = 2;
+//int pins1[4] = {43, 41, 39, 37};
+//int pins2[4] = {36, 38, 40, 42};
+//int pins3[4] = {44, 46, 47, 45};
+int pins1[PIN_NUM] = {9, 10};
+int pins2[PIN_NUM] = {11, 12};
+int pins3[PIN_NUM] = {13, 14};
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-  Serial.print("{");
-  for(int i=0; i<4; ++i) {
-    Serial.print(digitalRead(pins1[i]));
-    if(i!=3) { Serial.print(","); }
-  }
-  Serial.print("} {");
-  for(int i=0; i<4; ++i) {
-    Serial.print(digitalRead(pins2[i]));
-    if(i!=3) { Serial.print(","); }
-  }
-  Serial.print("} {");
-  for(int i=0; i<4; ++i) {
-    Serial.print(digitalRead(pins3[i]));
-    if(i!=3) { Serial.print(","); }
-  }
-  Serial.println("}");
+    Serial.print("{");
+    for(int i=0; i<PIN_NUM; ++i) {
+        Serial.print(analogRead(pins1[i]));
+        if(i!=1) { Serial.print(","); }
+    }
+    Serial.print("} {");
+    for(int i=0; i<PIN_NUM; ++i) {
+        Serial.print(analogRead(pins2[i]));
+        if(i!=1) { Serial.print(","); }
+    }
+    Serial.print("} {");
+    for(int i=0; i<PIN_NUM; ++i) {
+        Serial.print(analogRead(pins3[i]));
+        if(i!=1) { Serial.print(","); }
+    }
+    Serial.println("}");
 }
