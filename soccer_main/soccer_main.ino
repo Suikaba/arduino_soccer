5c0d171a769f4ffd1631d5583c48cd5b1a37dae9
Suikaba <suibaka@hotmail.co.jp>
2014-01-30 17:42:05 +0900

    Bug fix

--- soccer_main/soccer_main.ino
+++ soccer_main/soccer_main.ino
@@ -15,7 +15,7 @@ const int LED1    = 49;
 const int LED2    = 51;
 const int LED3    = 53;
 const int NEAR_IR_PIN[] = {0, 1, 2, 8, 4, 5, 6, 7};
-const int LIGHT_PIN[3][4] = {{43, 41, 39, 37}, {36, 38, 40, 42}, {44, 46, 47, 45}};
+const int LIGHT_PIN[3][2] = {{9, 10}, {11, 12}, {13, 14}};
 
 
 const int FAR_IR_ADDRESS = 0x10;
@@ -37,6 +37,7 @@ const int COMPASS_RANGE = 150;
 void calc_compass_reference();
 int read_compass();
 int compass_from_reference();
+void read_switches();
 void adjust_direction(int from_reference);
 void read_ping();
 void read_near_ir();
@@ -44,6 +45,7 @@ void read_far_ir();
 void read_light_sensor();
 int get_direction_from_near_ir();
 int get_direction_from_far_ir();
+void output_motor_power(int, int, int, int);
 
 
 DaisenMotorBoard motor_board;
@@ -53,15 +55,19 @@ int ping_value[PING_PIN_NUM] = {0};
 unsigned long near_ir_value[NEAR_IR_NUM] = {0};
 int first_nearest_ir = -1;
 int compass_reference = 0;
+int light_reference[3][2] = {};
 int largest_far_ir = -1;
 int far_ir_status = -1;
+bool switches[2] = {false};
 bool is_on_line[3] = {false};
 
 
 void setup(){
     Wire.begin();
+    pinMode(SWITCH1, INPUT);
+    pinMode(SWITCH2, INPUT);
     compass = HMC5883L();
-    Serial.begin(9600);
+    //Serial.begin(9600);
 
     int error = compass.set_scale(1.3);
     if(error != 0) {
@@ -78,6 +84,12 @@ void setup(){
     delay(50);
     read_compass(); // ignore first value
     calc_compass_reference();
+    
+    for(int i=0; i<3; ++i) {
+        for(int j=0; j<2; ++j) {
+            light_reference[i][j] = analogRead(LIGHT_PIN[i][j]);
+        }
+    }
 }
 
 void loop(){
@@ -106,18 +118,22 @@ void loop(){
         go_left[0]=50; go_left[1]=-49; go_left[2]=-50; go_left[3]=50;
         go_right[0]=-51; go_right[1]=50; go_right[2]=50; go_right[3]=-50;
         go_left_front[0]=0; go_left_front[1]=-50; go_left_front[2]=-50; go_left_front[3]=0;
-        go_right_front[0]=-55; go_right_front[1]=0; go_right_front[2]=0; go_right_front[3]=-52;
+        go_right_front[0]=-51; go_right_front[1]=0; go_right_front[2]=0; go_right_front[3]=-56;
         go_left_back[0]=50; go_left_back[1]=0; go_left_back[2]=0; go_left_back[3]=55;
-        go_right_back[0]=0; go_right_back[1]=50; go_right_back[2]=55; go_right_back[3]=0;
+        go_right_back[0]=0; go_right_back[1]=50; go_right_back[2]=51; go_right_back[3]=0;
     }
 
+    read_switches();
     const int from_reference = compass_from_reference();
     read_far_ir();
     read_light_sensor();
             
-    if(false/*is_on_line[0] || is_on_line[1] || is_on_line[2]*/) {
+    if(!switches[0] || !switches[1]) {
+        // don't anything
+        motor_board.set_motor_value(motor_stop);
+    } else if(false/*is_on_line[0] || is_on_line[1] || is_on_line[2]*/) {
         // light process
-    } else if(abs(from_reference) >= 750) {
+    } else if(abs(from_reference) >= 900) {
         adjust_direction(from_reference);
         lcd.clear();
     } else if(far_ir_status == 0) {
@@ -156,79 +172,80 @@ void loop(){
         }
     } else if(far_ir_status >= 5) {
         delay(5);
+        read_ping();
         read_near_ir();
-        const int compass_offset = from_reference/45;
+        const int compass_offset = from_reference/37;
         const int dir = get_direction_from_near_ir();
         switch(dir) {
         case FRONT_DIRECTION:
             motor_board.set_motor_value(
-                  go_front[0]+compass_offset,
-                  go_front[1]-compass_offset,
-                  go_front[2]+compass_offset,
-                  go_front[3]-compass_offset);
+                  go_front[0]-compass_offset,
+                  go_front[1]+compass_offset,
+                  go_front[2]-compass_offset,
+                  go_front[3]+compass_offset);
             lcd.clear();
             lcd.print("front n");
             break;
         case BACK_DIRECTION:
             // ignore offset
             motor_board.set_motor_value(
-                go_back[0],
-                go_back[1],
-                go_back[2],
-                go_back[3]);
+                go_back[0]-compass_offset,
+                go_back[1]+compass_offset,
+                go_back[2]-compass_offset,
+                go_back[3]+compass_offset);
             lcd.clear();
             lcd.print("back n");
             break;
         case LEFT_DIRECTION:
             motor_board.set_motor_value(
-                go_left[0]+compass_offset,
-                go_left[1]-compass_offset,
-                go_left[2]+compass_offset,
-                go_left[3]-compass_offset);
+                go_left[0]-compass_offset,
+                go_left[1]+compass_offset,
+                go_left[2]-compass_offset,
+                go_left[3]+compass_offset);
             lcd.clear();
             lcd.print("left n");
             break;
         case RIGHT_DIRECTION:
             motor_board.set_motor_value(
-                go_right[0]+compass_offset,
-                go_right[1]-compass_offset,
-                go_right[2]+compass_offset,
-                go_right[3]-compass_offset);
+                go_right[0]-compass_offset,
+                go_right[1]+compass_offset,
+                go_right[2]-compass_offset,
+                go_right[3]+compass_offset);
             lcd.clear();
             lcd.print("right n");
             break;
         case LEFT_FRONT_DIRECTION:
             motor_board.set_motor_value(
                 0,
-                go_left_front[1]-compass_offset,
-                go_left_front[2]+compass_offset,
+                go_left_front[1]+compass_offset,
+                go_left_front[2]-compass_offset,
                 0);
             lcd.clear();
             lcd.print("left_front n");
             break;
         case RIGHT_FRONT_DIRECTION:
             motor_board.set_motor_value(
-                go_right_front[0]+compass_offset,
+                go_right_front[0]-compass_offset,
                 0,
                 0,
-                go_right_front[4]-compass_offset);
+                go_right_front[3]+compass_offset);
             lcd.clear();
             lcd.print("right_front n");
             break;
         case LEFT_BACK_DIRECTION:
             motor_board.set_motor_value(
-                go_left_back[0]-compass_offset,
+                go_left_back[0]+(compass_offset/2),
                 0,
                 0,
-                go_left_back[4]+compass_offset);
+                go_left_back[3]-(compass_offset/2));
             lcd.clear();
             lcd.print("left_back n");
             break;
         case RIGHT_BACK_DIRECTION:
             motor_board.set_motor_value(
                 0,
-                go_right_back[1]-(compass_offset/2),
-                go_right_back[2]+(compass_offset/2),
+                go_right_back[1]+(compass_offset/2),
+                go_right_back[2]-(compass_offset/2),
                 0);
             lcd.clear();
             lcd.print("right_back n");
@@ -239,51 +256,50 @@ void loop(){
           lcd.clear();
         }
     } else {
-        const int compass_offset = from_reference/45;
+        const int compass_offset = from_reference/37;
         const int dir = get_direction_from_far_ir();
         switch(dir) {
         case FRONT_DIRECTION:
             motor_board.set_motor_value(
-                go_front[0]+compass_offset,
-                go_front[1]-compass_offset,
-                go_front[2]+compass_offset,
-                go_front[3]-compass_offset);
+                go_front[0]-compass_offset,
+                go_front[1]+compass_offset,
+                go_front[2]-compass_offset,
+                go_front[3]+compass_offset);
             lcd.clear();
             lcd.print("front far");
             break;
         case BACK_DIRECTION:
             motor_board.set_motor_value(
-                // ignore offset
-                go_back[0],
-                go_back[1],
-                go_back[2],
-                go_back[3]);
+                go_back[0]-compass_offset,
+                go_back[1]+compass_offset,
+                go_back[2]-compass_offset,
+                go_back[3]+compass_offset);
             lcd.clear();
             lcd.print("back far");
             break;
         case LEFT_DIRECTION:
             motor_board.set_motor_value(
-                go_left[0]+compass_offset,
-                go_left[1]-compass_offset,
-                go_left[2]+compass_offset,
-                go_left[3]-compass_offset);
+                go_left[0]-compass_offset,
+                go_left[1]+compass_offset,
+                go_left[2]-compass_offset,
+                go_left[3]+compass_offset);
             lcd.clear();
             Serial.println("left far");
             break;
         case RIGHT_DIRECTION:
             motor_board.set_motor_value(
-                go_right[0]+compass_offset,
-                go_right[1]-compass_offset,
-                go_right[2]+compass_offset,
-                go_right[3]-compass_offset);
+                go_right[0]-compass_offset,
+                go_right[1]+compass_offset,
+                go_right[2]-compass_offset,
+                go_right[3]+compass_offset);
             lcd.clear();
             lcd.print("right far");
             break;
         case LEFT_FRONT_DIRECTION:
             motor_board.set_motor_value(
                 0,
-                go_left_front[1]-compass_offset,
-                go_left_front[2]+compass_offset,
+                go_left_front[1]+compass_offset,
+                go_left_front[2]-compass_offset,
                 0);
             lcd.clear();
             lcd.print("left_front far");
@@ -293,24 +309,24 @@ void loop(){
                 go_right_front[0]+compass_offset,
                 0,
                 0,
-                go_right_front[4]-compass_offset);
+                go_right_front[3]-compass_offset);
             lcd.clear();
             lcd.print("right_front far");
             break;
         case LEFT_BACK_DIRECTION:
             motor_board.set_motor_value(
-                go_left_back[0]+compass_offset,
+                go_left_back[0]+(compass_offset/2),
                 0,
                 0,
-                go_left_back[4]-compass_offset);
+                go_left_back[3]-(compass_offset/2));
             lcd.clear();    
             lcd.print("left_back far");
             break;
         case RIGHT_BACK_DIRECTION:
             motor_board.set_motor_value(
                 0,
-                go_right_back[1]-compass_offset,
-                go_right_back[2]+compass_offset,
+                go_right_back[1]+(compass_offset/2),
+                go_right_back[2]-(compass_offset/2),
                 0);
             lcd.clear();
             lcd.print("right_back far");
@@ -352,12 +368,17 @@ int compass_from_reference() {
     }
 }
 
+void read_switches() {
+    switches[0] = digitalRead(SWITCH1);
+    switches[1] = digitalRead(SWITCH2);
+}
+
 void adjust_direction(int from_reference) {
-    const int offset = from_reference/50;
+    const int offset = from_reference/60;
     if(from_reference > COMPASS_RANGE) {
-        motor_board.set_motor_value(-14-offset, 14+offset, -14-offset, 14+offset);
+        motor_board.set_motor_value(-10-offset, 10+offset, -10-offset, 10+offset);
     } else if(from_reference < -COMPASS_RANGE) {
-        motor_board.set_motor_value(14-offset, -14+offset, 14-offset, -14+offset);
+        motor_board.set_motor_value(10-offset, -10+offset, 10-offset, -10+offset);
     } else {
         motor_board.set_motor_value(0, 0, 0, 0);
     }
@@ -406,8 +427,9 @@ void read_light_sensor()
 {
     for(int i=0; i<3; ++i) {
         is_on_line[i] = false;
-        for(int j=0; j<4; ++j) {
-            if(!is_on_line[i] && digitalRead(LIGHT_PIN[i][j]) == 0) {
+        for(int j=0; j<2; ++j) {
+            int tmp = analogRead(LIGHT_PIN[i][j]);
+            if(light_reference[i][j]-tmp > 150) {
                 is_on_line[i] = true;
             }
         }
@@ -417,7 +439,7 @@ void read_light_sensor()
 int get_direction_from_near_ir()
 {
     if(first_nearest_ir == 0) {
-        if((near_ir_value[1] >= 20 && near_ir_value[2] >= 20) || abs(near_ir_value[1]-near_ir_value[2]) < 6) {
+        if((near_ir_value[1] >= 30 && near_ir_value[2] >= 30) || abs(near_ir_value[1]-near_ir_value[2]) < 10) {
             return FRONT_DIRECTION;
         } else if(near_ir_value[1] < near_ir_value[2]) {
             return LEFT_FRONT_DIRECTION;
@@ -425,7 +447,7 @@ int get_direction_from_near_ir()
             return RIGHT_FRONT_DIRECTION;
         }
     } else if(first_nearest_ir == 1) {
-        if(near_ir_value[0] >= 20 && near_ir_value[3] >= 20) {
+        if(near_ir_value[0] <= 30 && near_ir_value[3] <= 30) {
             return LEFT_DIRECTION;
         } else if(near_ir_value[0] < near_ir_value[3]) {
             return LEFT_FRONT_DIRECTION;
@@ -433,20 +455,37 @@ int get_direction_from_near_ir()
             return LEFT_DIRECTION;
         }
     } else if(first_nearest_ir == 2) {
-        return RIGHT_DIRECTION;
+        if(near_ir_value[0] <= 30 && near_ir_value[4] <= 30) {
+            return RIGHT_DIRECTION;
+        } else if(near_ir_value[0] < near_ir_value[4]) {
+            return RIGHT_FRONT_DIRECTION;
+        } else {
+            return RIGHT_DIRECTION;
+        }
     } else if(first_nearest_ir == 3) {
         return LEFT_BACK_DIRECTION;
     } else if(first_nearest_ir == 4) {
         return RIGHT_BACK_DIRECTION;
     } else if(first_nearest_ir == 5) {
-        return BACK_DIRECTION;
+        if(near_ir_value[first_nearest_ir]-near_ir_value[3]) {
+            return LEFT_BACK_DIRECTION;
+        } else {
+            return BACK_DIRECTION;
+        }
     } else if(first_nearest_ir == 6) {
         return BACK_DIRECTION;
     } else if(first_nearest_ir == 7) {
-        return BACK_DIRECTION;
+        const int sub_between_nexts = abs(near_ir_value[5]-near_ir_value[6]);
+        if(near_ir_value[5] <= 30 && near_ir_value[6] <= 30 && sub_between_nexts < 13) {
+            if(ping_value[2] < ping_value[3]) {
+                return RIGHT_BACK_DIRECTION;
+            } else {
+                return LEFT_BACK_DIRECTION;
+            }
+        } else { // todo
+            return BACK_DIRECTION;
+        }
     }
-    return -1;
-    lcd.print("hoge");
 }
 
 int get_direction_from_far_ir()
@@ -473,6 +512,7 @@ int get_direction_from_far_ir()
     return -1;
 }
 
+// debug
 void output_motor_power(int a, int b, int c, int d) {
     Serial.print("motor_power:");
     Serial.print(a); Serial.print(" ");
