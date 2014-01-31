2686da444611341dea29760c9b6b947269b59add
Suikaba <suibaka@hotmail.co.jp>
2014-02-01 06:10:08 +0900

    Implement light_sensor

--- light_sensor/light_sensor.ino
+++ light_sensor/light_sensor.ino
@@ -3,31 +3,43 @@ const int PIN_NUM = 2;
 //int pins1[4] = {43, 41, 39, 37};
 //int pins2[4] = {36, 38, 40, 42};
 //int pins3[4] = {44, 46, 47, 45};
-int pins1[PIN_NUM] = {9, 10};
-int pins2[PIN_NUM] = {11, 12};
-int pins3[PIN_NUM] = {13, 14};
+const int LIGHT_PIN[3][2] = {{9, 10}, {11, 12}, {13, 14}};
 
-void setup()
-{
-    Serial.begin(9600);
+bool is_on_line[3] = {false};
+int light_reference[3][2] = {0};
+
+void read_light() {
+    for(int i=0; i<3; ++i) {
+        is_on_line[i] = false;
+        for(int j=0; j<2; ++j) {
+            const int tmp = analogRead(LIGHT_PIN[i][j]);
+            if(tmp - light_reference[i][j] > 150) {
+                is_on_line[i] = true;
+            }
+        }
+    } 
 }
 
-void loop()
-{
-    Serial.print("{");
-    for(int i=0; i<PIN_NUM; ++i) {
-        Serial.print(analogRead(pins1[i]));
-        if(i!=1) { Serial.print(","); }
+void setup() {
+    Serial.begin(9600);
+    for(int i=0; i<3; ++i) {
+        for(int j=0; j<2; ++j) {
+            pinMode(LIGHT_PIN[i][j], INPUT);
+        }
     }
-    Serial.print("} {");
-    for(int i=0; i<PIN_NUM; ++i) {
-        Serial.print(analogRead(pins2[i]));
-        if(i!=1) { Serial.print(","); }
+    delay(100);
+    for(int i=0; i<3; ++i) {
+        for(int j=0; j<2; ++j) {
+            light_reference[i][j] = analogRead(LIGHT_PIN[i][j]);
+        }
     }
-    Serial.print("} {");
-    for(int i=0; i<PIN_NUM; ++i) {
-        Serial.print(analogRead(pins3[i]));
-        if(i!=1) { Serial.print(","); }
+}
+
+void loop() {
+    read_light();
+    for(int i=0; i<3; ++i) {
+        Serial.print(is_on_line[i]);
+        Serial.print(" ");
     }
-    Serial.println("}");
+    Serial.println();
 }
