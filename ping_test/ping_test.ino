7075b1b43b63eec81e971c7c5dfed603f2662d34
Suikaba <suibaka@hotmail.co.jp>
2014-02-02 18:25:32 +0900

    Add ping test

---
+++ ping_test/ping_test.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+
+const int PING_ADDRESS = 0x16;
+
+void setup() {
+    Wire.begin();
+}
+
+void loop() {
+    int ping_value[4] = {0};
+    Wire.requestFrom(PING_ADDRESS, 4);
+    int i=0;
+    while(Wire.available() && i < 4) {
+        ping_value[i] = Wire.read();
+        ++i;
+    }
+    Wire.endTransmission();
+    delay(20);
+}
